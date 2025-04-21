a108ea9d04892dedbe916c07f8e40ec112fcd46c
Julian David Hoyos <juliand.hoyos@gmail.com>
2025-04-20 20:22:27 -0500

    Ejercicio3 principios

---
+++ src/main/java/org/tallerprincipios/ejercicio3/sinaplicar/EmpleadoTodero.java
@@ -0,0 +1,14 @@
+package org.tallerprincipios.ejercicio3.sinaplicar;
+
+public class EmpleadoTodero {
+
+  public void atenderCliente() {
+    System.out.println("Atendiendo al cliente...");
+  }
+  public void prepararComida() {
+    System.out.println("Preparando la comida...");
+  }
+  public void cobrar() {
+    System.out.println("Cobrando al cliente...");
+  }
+}
